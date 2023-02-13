c8ae4a1e21a9d1218833ebcc999496c28b618b30
raphiniert <dev@raphiniert.eu>
2023-02-13 20:31:20 +0100

    connect to db on startup, disconnect on shotdown

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,8 +1,32 @@
+import logging
+
 from fastapi import FastAPI
+from rich.logging import RichHandler
+
+from api.db.database import database
+
+
+FORMAT = "%(message)s"
+logging.basicConfig(
+    level="DEBUG", format=FORMAT, datefmt="%Y-%m-%d %H:%M:%S", handlers=[RichHandler()]
+)
+
+logger = logging.getLogger("fastapi-demo")
 
 app = FastAPI()
 
 
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+
 @app.get("/")
 async def root():
+    logger.info("Hello World")
     return {"message": "Hello World"}
