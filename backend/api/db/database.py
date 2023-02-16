71b7a67755787a8948cad5680118dc6cd2b2ab72
raphiniert <dev@raphiniert.eu>
2023-02-16 21:43:55 +0100

    setup async db connection, engine and local session

--- backend/api/db/database.py
+++ backend/api/db/database.py
@@ -1,6 +1,4 @@
-import databases
-
-from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -9,4 +7,19 @@ from api.core.config import settings
 
 DATABASE_URL = f"postgresql+asyncpg://{settings.db_user}:{settings.db_password.get_secret_value()}@{settings.db_server}:{settings.db_port}/{settings.db_db}"
 
-database = databases.Database(DATABASE_URL)
+async_engine = create_async_engine(
+    DATABASE_URL,
+    # echo=True,
+)
+
+AsyncSessionLocal = sessionmaker(
+    async_engine, class_=AsyncSession, expire_on_commit=False
+)
+
+Base = declarative_base()
+
+
+async def get_db():
+    async with AsyncSessionLocal() as db:
+        yield db
+        await db.commit()
