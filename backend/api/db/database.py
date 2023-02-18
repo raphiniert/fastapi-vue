4772df27be3657e2cee04085dab0badc8ca1b5a5
raphiniert <dev@raphiniert.eu>
2023-02-18 23:33:51 +0100

    resturcture import statements

--- backend/api/db/database.py
+++ backend/api/db/database.py
@@ -1,6 +1,5 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 from api.core.config import settings
 
