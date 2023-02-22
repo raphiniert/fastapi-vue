ce45e6db31496ab853bd458c7f5aede35a88a541
raphiniert <dev@raphiniert.eu>
2023-02-22 23:08:08 +0100

    added event loop fixture to prevent RuntimeErrors

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,4 +1,6 @@
+import asyncio
 import pytest
+
 from fastapi.testclient import TestClient
 from typing import Generator
 
@@ -9,6 +11,13 @@ from api.main import app
 @pytest.fixture(scope="session")
 def db() -> Generator:
     yield AsyncSessionLocal()
+
+
+@pytest.fixture(scope="session")
+def event_loop() -> Generator:
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
 
 
 @pytest.fixture(scope="module")
