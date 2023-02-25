f7690f086a6c468debc30cfcdb1827ce4899d54c
raphiniert <dev@raphiniert.eu>
2023-02-25 12:09:39 +0100

    removed commented drop all

--- backend/api/main.py
+++ backend/api/main.py
@@ -25,7 +25,6 @@ app.include_router(demos.router, prefix="/v1")
 @app.on_event("startup")
 async def startup():
     async with async_engine.begin() as conn:
-        # await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
 
